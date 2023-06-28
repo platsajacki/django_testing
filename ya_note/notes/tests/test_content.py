496f30f125da1998a366983b516d3da2a55b57fc
Вячеслав Менюхов <menyukhov@bk.ru>
2023-06-28 20:34:37 +0300

    Update ya_note/tests

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -8,35 +8,27 @@ User = get_user_model()
 
 
 class TestContent(TestCase):
-    TITLE = 'Заметка'
-    TEXT = 'Текст'
-    SLUG = 'note'
-    SLUG2 = 'note2'
     NOTES_LIST = reverse('notes:list')
     ADD_NOTE_PAGE = reverse('notes:add')
 
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='Автор')
-        cls.another_user = User.objects.create(username='Пользователь')
-        cls.note_author = Note.objects.create(
-            title=cls.TITLE, text=cls.TEXT, slug=cls.SLUG, author=cls.author
+        cls.reader = User.objects.create(username='Читатель')
+        cls.note = Note.objects.create(
+            title='Заметка', text='Текст', slug='note', author=cls.author
         )
-        cls.note_user = Note.objects.create(
-            title=cls.TITLE, text=cls.TEXT,
-            slug=cls.SLUG2, author=cls.another_user
-        )
-        cls.edit_url = reverse('notes:edit', args=(cls.note_author.slug,))
+        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
 
     def test_note_in_list_of_notes(self):
         self.client.force_login(self.author)
         response = self.client.get(self.NOTES_LIST)
-        self.assertIn(self.note_author, response.context['object_list'])
+        self.assertIn(self.note, response.context['object_list'])
 
     def test_elses_note_in_list_of_notes(self):
-        self.client.force_login(self.author)
+        self.client.force_login(self.reader)
         response = self.client.get(self.NOTES_LIST)
-        self.assertNotIn(self.note_user, response.context['object_list'])
+        self.assertNotIn(self.note, response.context['object_list'])
 
     def test_authorized_client_has_form(self):
         self.client.force_login(self.author)
--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -4,7 +4,6 @@ from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
 from django.test import Client, TestCase
 from django.urls import reverse
-from pytils.translit import slugify
 
 from ..models import Note
 
@@ -12,10 +11,6 @@ User = get_user_model()
 
 
 class TestContent(TestCase):
-    TITLE = 'Заметка'
-    TEXT = 'Текст'
-    SLUG = 'note'
-    SLUG2 = 'note2'
     ADD_NOTE_PAGE = reverse('notes:add')
     DONE_PAGE = reverse('notes:success')
 
@@ -24,45 +19,45 @@ class TestContent(TestCase):
         cls.author = User.objects.create(username='Автор')
         cls.client_author = Client()
         cls.client_author.force_login(cls.author)
-        cls.another_user = User.objects.create(username='Пользователь')
-        cls.client_another_user = Client()
-        cls.client_another_user.force_login(cls.another_user)
-        cls.note_author = Note.objects.create(
-            title=cls.TITLE, text=cls.TEXT, slug=cls.SLUG, author=cls.author
+        cls.reader = User.objects.create(username='Читатель')
+        cls.client_reader = Client()
+        cls.client_reader.force_login(cls.reader)
+        cls.note = Note.objects.create(
+            title='Заметка', text='Текст', slug='note', author=cls.author
         )
         cls.note_without_slug = Note.objects.create(
-            title=cls.TITLE, text=cls.TEXT, author=cls.author
+            title='Заметка', text='Текст', author=cls.author
         )
         cls.form_data = {
-            'title': cls.TITLE,
-            'text': cls.TEXT,
-            'slug': cls.SLUG2
+            'title': 'Заметка',
+            'text': 'Текст',
+            'slug': 'note2'
         }
-        cls.edit_url = reverse('notes:edit', args=(cls.note_author.slug,))
-        cls.delete_url = reverse('notes:delete', args=(cls.note_author.slug,))
+        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
+        cls.delete_url = reverse('notes:delete', args=(cls.note.slug,))
 
     def test_slug_unique(self):
         with self.assertRaises(IntegrityError):
             self.note_with_non_unique_slug = Note.objects.create(
-                title=self.TITLE, text=self.TEXT,
-                slug=self.SLUG, author=self.author
+                title='Заметка', text='Текст',
+                slug='note', author=self.author
             )
 
     def test_anonymous_user_cant_create_note(self):
         before_count_note = Note.objects.count()
-        self.client.post(self.ADD_NOTE_PAGE, data=self.form_data)
+        response = self.client.post(self.ADD_NOTE_PAGE, data=self.form_data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
         after_count_note = Note.objects.count()
         self.assertEqual(before_count_note, after_count_note)
 
     def test_auth_user_can_create_note(self):
         before_count_note = Note.objects.count()
-        self.client_author.post(self.ADD_NOTE_PAGE, data=self.form_data)
+        response = self.client_author.post(
+            self.ADD_NOTE_PAGE, data=self.form_data
+        )
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
         after_count_note = Note.objects.count() - 1
         self.assertEqual(before_count_note, after_count_note)
-
-    def test_unique_slug(self):
-        translit = slugify(self.TITLE)
-        self.assertEqual(translit, self.note_without_slug.slug)
 
     def test_user_can_edit_their_notes(self):
         response = self.client_author.post(self.edit_url, data=self.form_data)
@@ -72,12 +67,12 @@ class TestContent(TestCase):
         response = self.client_author.delete(self.delete_url)
         self.assertRedirects(response, self.DONE_PAGE)
 
-    def test_another_user_can_edit_their_notes(self):
-        response = self.client_another_user.post(
+    def test_reader_can_edit_their_notes(self):
+        response = self.client_reader.post(
             self.edit_url, data=self.form_data
         )
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
-    def test_another_user_can_edit_their_notes(self):
-        response = self.client_another_user.post(self.delete_url)
+    def test_reader_can_edit_their_notes(self):
+        response = self.client_reader.post(self.delete_url)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)

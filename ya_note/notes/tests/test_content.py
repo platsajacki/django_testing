cc4a6ae75fa0589ac117321fd311f95e4741ffd5
Вячеслав Менюхов <menyukhov@bk.ru>
2023-07-01 21:33:44 +0300

    Update TestContent

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
 
 from ..models import Note
@@ -16,28 +16,29 @@ class TestContent(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='Автор')
+        cls.client_author = Client()
+        cls.client_author.force_login(cls.author)
         cls.reader = User.objects.create(username='Читатель')
+        cls.client_reader = Client()
+        cls.client_reader.force_login(cls.reader)
         cls.note = Note.objects.create(
             title='Заметка', text='Текст', slug='note', author=cls.author
         )
         cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
 
     def test_note_in_list_of_notes(self):
-        self.client.force_login(self.author)
-        response = self.client.get(self.NOTES_LIST)
+        response = self.client_author.get(self.NOTES_LIST)
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertIn(self.note, response.context['object_list'])
 
     def test_elses_note_in_list_of_notes(self):
-        self.client.force_login(self.reader)
-        response = self.client.get(self.NOTES_LIST)
+        response = self.client_reader.get(self.NOTES_LIST)
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertNotIn(self.note, response.context['object_list'])
 
     def test_authorized_client_has_form(self):
-        self.client.force_login(self.author)
         for url in (self.ADD_NOTE_PAGE, self.edit_url):
             with self.subTest(name=url):
-                response = self.client.get(url)
+                response = self.client_author.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
                 self.assertIn('form', response.context)
